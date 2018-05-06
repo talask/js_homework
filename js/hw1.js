670ce077915702cfada5def3dddacf2ee0f6f323
talask <natasha169@ukr.net>
2018-05-06 21:39:49 +0300

    homework

---
+++ js/hw1.js
@@ -0,0 +1,123 @@
+// ***********************
+// *     ПЕРЕМЕННЫЕ      *
+// ***********************
+console.log( "ПЕРЕМЕННЫЕ" );
+let price = "";
+const max = 999;
+const name = "Nafanya";
+let age = "";
+
+console.log(test);
+var test = 'string';
+// Ответ: undefined
+
+var x = 'string';
+var x = 'string №2';
+console.log(x);
+//  Ответ: string №2
+
+// console.log(text);
+// let text='string';
+// Ответ: Error: text is not defined
+
+// const y = 'string';
+//       y = 'string №2';
+// console.log(y);
+// Ответ: Error: Assignment to constant variable.
+
+// let someVariable = 15;
+// let someVariable = 10;
+//  Ответ: Error: Identifier 'someVariable' has already been declared
+
+// ***********************
+// *       СТРОКИ        *
+// ***********************
+console.log( "СТРОКИ" );
+let string = 'some test string';
+console.log( "STRING - " +  string);
+// 1
+console.log(string[0] + " " + string.slice(-1));
+
+// 2
+console.log(string[0].toUpperCase() + string.slice(1,(string.length - 1)) + string.slice(-1).toUpperCase());
+
+// 3
+console.log(string.indexOf('string'));
+
+// 4
+let index = string.indexOf(" ") + 1;
+console.log(string.indexOf(" ",index));
+
+// 5
+console.log("==" + string.substr(5, 4) + "===");
+
+// 6 
+console.log("==" + string.slice(5, 9) + "===");
+
+// 7
+string = string.slice(0, string.length - 6);
+console.log("==" + string + "===");
+
+// 8
+let a = 20;
+let b = 16;
+console.log(a + '' + b);
+
+// ***********************
+// *       ЧИСЛА         *
+// ***********************
+console.log( "ЧИСЛА" );
+// 1 
+console.log(Math.PI.toFixed(2));
+
+// 2
+console.log("15,11,16,12,51,12,13,51");
+console.log("MIN - " + Math.min(15,11,16,12,51,12,13,51));
+console.log("MAX - " + Math.max(15,11,16,12,51,12,13,51));
+
+// 3
+console.log(Math.random().toFixed(2));
+let min_random = 0,
+    max_random = 987;
+console.log(Math.floor(Math.random() * max_random));
+
+// 4
+let summa = 0.6 +0.7;
+let summa1 = (0.6*10 + 0.7*10)/10;
+console.log(summa.toFixed(1));
+console.log(summa1);
+
+// 5 
+console.log(parseInt('100$'));
+
+// ***********************
+// *       ОБЪЕКТЫ        *
+// ***********************
+console.log( "ОБЪЕКТЫ" );
+// 1
+let obj = {
+	product: 'iphone'
+};
+
+// 2
+obj['price'] = '1000';
+obj['currency'] = 'dollar';
+
+// 3
+obj.detalis = {
+	model: 'iPhone 7 Plus',
+	color: 'white'
+}
+
+for (var key in obj) {
+  if (typeof(obj[key]) === 'object') {
+    console.log( "Ключ: " + key + " значение: " );
+    for (var key1 in obj[key]) {
+      console.log( key1 + " значение: " + obj[key][key1] );
+    }
+  }
+  else{
+    console.log( "Ключ: " + key + " значение: " + obj[key] );
+  }
+}
+  
---
+++ js/hw2.js
@@ -0,0 +1,80 @@
+// ***********************
+// *     присваивание    *
+// ***********************
+
+//1
+let a = 1, b = 2, c = 3, x = 4, y = 5, z = 6, i = 7;
+ a += 10, b *= 18,  c -= 10
+ x += a, y *= z,  i *= 5 * z
+console.log( "a = " + a + ", b = " + b + ", c = " + c + ", x = " + x + ", y = " + y + ", i = " + i );
+ //2
+ let q = 2;
+ q *= q 
+ console.log( "q = " + q);
+
+// ****************************
+// * АРИФМЕТИЧЕСКИЕ ОПЕРАТОРЫ *
+// ****************************
+//1
+let aa = 5; 
+console.log( aa++ ); // 5
+console.log( ++aa ); // 7
+//2
+let bb=10;
+if ( bb%2 === 0) {
+  console.log(bb + " - число четное")
+}
+else {
+  console.log(bb + " - число нечетное") 
+}
+
+// ****************************
+// *    УСЛОВНЫЕ ОПЕРАТОРЫ    *
+// ****************************
+
+//1
+let variable = 'hidden';
+if(variable === 'visible') {
+  variable = 'hidden';
+}
+else {
+  variable = 'visible';
+}
+console.log(variable);
+
+variable = variable === 'visible' ? 'hidden' : 'visible';
+console.log(variable);
+
+//2
+let v = 2;
+if(v === 0) {
+  v = 1;
+}
+else {
+  if(v < 0) {
+      v = 'less then zero';
+  }
+  else {
+    v *= 10;  
+  }
+}
+console.log(v);
+
+v = v === 0 ? v = 1 :
+    v < 0 ? v = 'less then zero' : v *= 10;  
+console.log(v);
+
+// ****************************
+// *          SWITCH          *
+// ****************************
+
+let aaa = 'flex';
+switch(a) {
+  case 'block': console.log('block');
+    break;
+  case 'none':  console.log('none');
+    break;
+  case 'inline':  console.log('inline');
+    break;
+  default:  console.log('other');
+}
