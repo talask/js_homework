08ffd1a3cd8573e41fc022952ea2089b63e8a835
talask <natasha169@ukr.net>
2018-05-18 22:12:20 +0300

    test old

---
+++ js/test.js
@@ -0,0 +1,46 @@
+const fruits = [
+    {
+        name: "orange",
+        weight: 150
+    },
+    {
+        name: "pineapple",
+        weight: 500
+    },
+    {
+        name: "kiwi",
+        weight: 100
+    },
+    {
+        name: "apple",
+        weight: 250
+    },
+    {
+        name: "pear",
+        weight: 200
+    },
+    {
+        name: "mango",
+        weight: 300
+    },
+    {
+        name: "apple",
+        weight: 99
+    },
+    {
+        name: "grapefruit",
+        weight: 400
+    },
+    {
+        name: "grapefruit",
+        weight: 300
+    },
+    
+];
+
+for (let i of fruits) {
+    if (((i.name === 'apple') || (i.name === 'APPLE')) && (i.weight <= 100)) {
+        console.log(i.name + " " + i.weight);
+        break;
+    }
+}
