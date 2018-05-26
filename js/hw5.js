9f0d937961f1b6bde46d0e990052c65e0a1de9a7
talask <natasha169@ukr.net>
2018-05-26 20:24:31 +0300

    homework in work

--- js/hw5.js
+++ js/hw5.js
@@ -1,3 +1,4 @@
+// 1
 let array_string = ['i', 'like', 'tea'],
     array_number = ['7', '80', '65'],
     array_object = [{age: 45, name: 'Jhon'},{age: 20, name: 'Aaron'}],
@@ -39,3 +40,62 @@ console.log(arrayToString(array_number, multiplication));
 console.log(arrayToString(array_object, sentence));
 console.log(arrayToString(array_value, reverse));
 console.log(arrayToString(array_some_number, addition));
+// 2 Переписать ввиде своих фуекций методы every и some
+let numberArr = [1, 0, 22, 17, 2, 10, 15];
+let users = [
+  {
+    name: 'Denis',
+    age: 29,
+    balance: 250
+  },
+  {
+    name: 'Ivan',
+    age: 16,
+    balance: 100
+  }
+];
+
+let prov =  (item, el, arr) => item.age <= 10;
+ function some(arr, func) {
+    for (let i=0; i < arr.length; i++) {
+        if (func(arr[i], i, arr)) return 'true';
+    }
+    return 'false';
+ }
+
+
+ let resultSome = some(users, (item, el, arr) => item.age <= 10);
+ 
+ console.log('some ' + resultSome);
+
+ function every(arr, func) { 
+    for (let i=0; i < arr.length; i++) {
+        if (!func(arr[i], i, arr)) return 'false';
+    }
+    return 'true';
+ }
+
+ let resultEvery = every(users, (item, el, arr) => item.age >= 10);
+
+ console.log('every ' + resultEvery); 
+
+// 3  из презентации по массивам сделать задачи: 2, 3, 8, 10, 11
+// 2
+ let someArray = [10,7, 4, 2, 1, -2, -9];
+
+someArray.sort(function(prev, next) {
+    if (prev < next) return 1;
+    if (prev >= next) return -1;
+});
+
+console.log(someArray);
+
+// 3
+function makeArray(arr, begin, end){
+    let newArray = arr.slice(begin,end);
+    return newArray;
+}
+
+console.log(makeArray(someArray, 2, 5));
+
+// 8
